b3619131173e45f92b2af812e27934911b278aae
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-27 10:09:07 +0530

    server initailsed

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var mongoose = require('mongoose');
+const app = express()
+const port = 8000
+const cors = require('cors')
+
+mongoose.connect("mongodb://localhost:27017/assignment",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(require("./routes"));
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
+
+process.on('SIGINT', async function () {
+    await mongoose.disconnect();
+    process.exit(0)
+});
